871743639004d0f7f36bcc2f2d99cff7798b23fa
Amrit Raj <amrit.raj1504@gmail.com>
2023-10-08 14:12:41 +0530

    redis configuration changed

--- src/cache/redisClient.js
+++ src/cache/redisClient.js
@@ -1,5 +1,11 @@
 const { Redis } = require("ioredis");
+const redis = require("redis");
+const REDIS_PORT = process.env.REDIS_PORT || 6379
+const redisClient = redis.createClient(REDIS_PORT); 
 
-const redisClient = new Redis();
+redisClient.on('connect', () => console.log(`Redis is connected on ${REDIS_PORT}`))
+redisClient.on('error', (error) => console.log(error));
+
+// const redisClient = new Redis();
 
 module.exports = { redisClient }
