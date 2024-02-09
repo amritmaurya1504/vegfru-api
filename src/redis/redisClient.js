9fd77e0f8cbb45b9907f680b12a86c495f30dd8f
Amrit Raj <amrit.raj1504@gmail.com>
2024-02-09 20:24:02 +0530

    redis updated

--- src/redis/redisClient.js
+++ src/redis/redisClient.js
@@ -1,19 +1,19 @@
 const { createClient } = require('redis');
 
 // FOR PRODUCTION
-const client = createClient({
-    password: `${process.env.REDIS_PASSWORD}`,
-    socket: {
-        host: `${process.env.REDIS_URI}`,
-        port: process.env.REDIS_PORT
-    }
-});
+// const client = createClient({
+//     password: `${process.env.REDIS_PASSWORD}`,
+//     socket: {
+//         host: `${process.env.REDIS_URI}`,
+//         port: process.env.REDIS_PORT
+//     }
+// });
 
 // FOR DEVELOPEMT
-// const client = createClient({
-//     host: process.env.REDIS_HOST,
-//     port: process.env.REDIS_PORT
-// });
+const client = createClient({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT
+});
 
 (async () => {
     try {
